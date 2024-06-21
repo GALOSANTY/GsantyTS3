4c4c440956ff53de886c0ad462e49a3075556f25
GABRIELA <GABRIELA@DESKTOP-HQ6QSFP>
2024-06-21 10:10:01 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GsantyTS3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new Vistas.vLogin());
+        }
+    }
+}
